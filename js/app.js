2411c1dacc0d30a03935703912bdd3bbddab8769
gegebe <ggirbes@gmail.com>
2023-11-08 13:58:36 +0100

    liada parda

--- js/app.js
+++ js/app.js
@@ -1,6 +1,6 @@
 // Tu código aquí.
 
-import {createApp, ref,} from "https://unpkg.com/vue@3/dist/vue.esm-browser.js";
+import {createApp, ref, reactive} from "https://unpkg.com/vue@3/dist/vue.esm-browser.js";
 
 createApp({
   setup() {
@@ -8,14 +8,28 @@ createApp({
     //Equivale a la ID counter del <h1>>
     const counter = ref(0)
 
+    const classObj = reactive({
+      hasColor:"grey",
+      changes: ()=>{
+        if(hasColor > 0){
+          return "green"
+        }
+        if(hasColor < 0){
+          return "red";
+        }
+      }
+    })
+
     //Pasa el valor del HTML newCounter(-1), por el parámetro x
-    function newCounter(x){
+    function newCounter(x, classObj){
       counter.value = counter.value + x
+      classObj.changes(counter.value)
     }
 
     return {
       counter,
-      newCounter
+      newCounter,
+      classObj
 
     };
   },
