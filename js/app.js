c1e97febf3347947d449e19dc585e87a4d474bda
gegebe <ggirbes@gmail.com>
2023-11-08 11:32:46 +0100

    finished

--- js/app.js
+++ js/app.js
@@ -1,15 +1,22 @@
 // Tu código aquí.
 
-import {
-  createApp,
-  ref,
-} from "https://unpkg.com/vue@3/dist/vue.esm-browser.js";
+import {createApp, ref,} from "https://unpkg.com/vue@3/dist/vue.esm-browser.js";
 
 createApp({
   setup() {
-    const message = ref("Hello Vue!");
+    
+    //Equivale a la ID counter del <h1>>
+    const counter = ref(0)
+
+    //Pasa el valor del HTML newCounter(-1), por el parámetro x
+    function newCounter(x){
+      counter.value = counter.value + x
+    }
+
     return {
-      message,
+      counter,
+      newCounter
+
     };
   },
 }).mount("#app");
